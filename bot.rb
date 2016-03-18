661d75117da854cb6872ab9eb37b46d950cfc975
sarahincapie <saral85@gmail.com>
2016-03-18 11:08:31 -0400

    twitterbot

--- bot.rb
+++ bot.rb
@@ -4,26 +4,32 @@ require 'twitter'
 #!/usr/bin/env ruby
 
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "RLsXuDn3BsUY3HPyDt1INKYBj"
-  config.consumer_secret     = "YOKkfhexLNb39PZ3nfqMDdCWlz4UHOxKz757Uj3QfvInhDuaYZ"
-  config.access_token        = "18005113-IbaCfaHpcoiPx4jRKnWy1hXyujTTPGOWdDW8PkHxF"
-  config.access_token_secret = "dw4ZrmmDu71Z13CILyMqB9urPMse7ZZTyBHNxrbq9Gc5j"
+  config.consumer_key        = "XXX"
+  config.consumer_secret     = "XXX"
+  config.access_token        = "XXX"
+  config.access_token_secret = "XXX"
 end
 
 
+# // TWEETS FROM THE COMMAND LINE
+#client.update('Hello World! Again Tweeting from the command line :) ')
 
-#client.update('Hello World! Again Tweeting from the command line :) ') // TWEETS FROM THE COMMAND LINE 
 
-
-#client.search("#ruby").take(50).each do |tweet| // THIS MAKES A SEARCH 
+# // THIS MAKES A SEARCH 
+#client.search("#ruby").take(50).each do |tweet|
   #puts tweet.text
 #end
 
-search_options = {
-  result_type: "recent",
-  geocode: "25.7906532288,-80.1300430298,10mi"
-}
 
-client.search("cuba", search_options).take(50).each do |tweet|
-  puts "#{tweet.user.screen_name}: #{tweet.text}"
-end
+#// THIS MAKES A SEARCH WITH GEOCODE
+
+# search_options = {
+#   result_type: "recent",
+#   geocode: "25.7906532288,-80.1300430298,10mi"
+# }
+
+# client.search("cuba", search_options).take(50).each do |tweet|
+#   puts "#{tweet.user.screen_name}: #{tweet.text}"
+# end
+
+
